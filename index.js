a285e77f8d9125721d8ef9f16bdb0c6e676c60d7
jamu gan <jamu03031996@gmail.com>
2021-04-22 10:37:06 +0530

    nodejs 2nd  commit

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 //creating server using express js web framework
-const express = require('express')
-const app = express()
-const routing = require('./router.js')
-const port = 3000
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/"+"index.html")
-})
-app.get('/myprofile',(req,res)=>{
-    res.sendFile(__dirname+"/"+"profile.html")
-})
+// const express = require('express')
+// const app = express()
+// const routing = require('./router.js')
+// const port = 3000
+// app.get('/',(req,res)=>{
+//     res.sendFile(__dirname+"/"+"index.html")
+// })
+// app.get('/myprofile',(req,res)=>{
+//     res.sendFile(__dirname+"/"+"profile.html")
+// })
 
-app.get('/action_page',(req,res)=>{
-    console.log("actionpage");
-    res.send("<h1>hai "+req.query['email']+"</h1>")
-})
-//app.use('/',routing);
-app.use('/myprofile',routing);
-app.listen(port,()=>console.log(`server running on ${port}`))
+// app.get('/action_page',(req,res)=>{
+//     console.log("actionpage");
+//     res.send("<h1>hai "+req.query['email']+"</h1>")
+// })
+// //app.use('/',routing);
+// app.use('/myprofile',routing);
+// app.listen(port,()=>console.log(`server running on ${port}`))
 
 
 //connect cloud mongo db...
 // const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://admin:admin@cluster0.cflox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://@@@@@:*****@cluster0.cflox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 // client.connect(err => {
 //   client.db("myFirstDatabase").collection("profile");
@@ -53,20 +53,20 @@ app.listen(port,()=>console.log(`server running on ${port}`))
 // const circle = require('./circle.js');
 // console.log(`The area of a circle of radius 4 is ${circle.area(4)}`);
 
-    // //conect local mongo db...
-    //var MongoClient = require('mongodb').MongoClient;
-    // var url = 'mongodb://127.0.0.1:27017/';
-    // MongoClient.connect(url,{ useUnifiedTopology: true },function(err,db){
-    // if(err) throw err;
-    // var dbmy = db.db('mydbfirst');
-    // var mydata = {name: 'google.com',website:'www.google.com'};
+    //conect local mongo db...
+    var MongoClient = require('mongodb').MongoClient;
+    var url = 'mongodb://127.0.0.1:27017/';
+    MongoClient.connect(url,{ useUnifiedTopology: true },function(err,db){
+    if(err) throw err;
+    var dbmy = db.db('mydbfirst');
+    var mydata = {name: 'yahoo.co.in',website:'www.yahoo.co.in'};
 
-    // dbmy.collection('test3').insertOne(mydata,function(errs,res1){
-    //     if(errs) throw errs;
-    //     console.log('Document Inserted');
-    //     db.close();
-    // });
-    // });
+    dbmy.collection('test3').insertOne(mydata,function(errs,res1){
+        if(errs) throw errs;
+        console.log('Document Inserted');
+        db.close();
+    });
+    });
 
 // var MongoClient = require('mongodb').MongoClient;
 // var url = "mongodb://localhost:27017/";
--- server.js
+++ server.js
@@ -4,33 +4,19 @@ const port = 5000;
  //conect local mongo db...
     var MongoClient = require('mongodb').MongoClient;
     var url = 'mongodb://127.0.0.1:27017/';
-    // MongoClient.connect(url,{ useUnifiedTopology: true },function(err,db){
-    // if(err){
-    //   throw err
-    // }else{
-    //   app.get("/", (req, res) => {
-    //     var dbo = db.db("mydbfirst");
-    //     dbo.collection("test2").find()
-    //     res.send(dbo);
-    //   });
-    // }
-    // });
-
-
     app.get('/', (req, res) => {
-      MongoClient.connect(url,{ useUnifiedTopology: true },function(err, db) {
-          if (err) throw err;
+      MongoClient.connect(url,{ useUnifiedTopology: true },function(error, db) {
+          if (error) throw error;
           var dbo = db.db("mydbfirst");
-          dbo.collection("test3").findOne(
-          function(err, result) {
-              if (err) throw err;
-              res.json(result);
-              db.close();
-          });
+          dbo.collection('test3').find({}).toArray(function (err, result) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(result);
+            }
+        })
       });
   });
-
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
